5abe5f7376c56e1be4ec24d521dbf25877571dbd
Cesar Ramirez <LS19070100@jerez.tecnm.mx>
2020-11-19 22:56:12 -0600

    Se añadio dio codigo para llenar el vector con 1000 datos aleatorios, ademas se añadieron los contadores para los intercambios y comparaciones, ademas de un menu que estara sujeto a modificaciones para el resto de los metodos de ordenamiento

--- src/PruebaMetodosDeOrdenamiento.java
+++ src/PruebaMetodosDeOrdenamiento.java
@@ -1,14 +1,47 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+interface Correcion{
+	Scanner input = new Scanner(System.in);
+	
+	public static int validacion() {
+		int r = 0;
+		boolean e = false;
+		do {
+			try {
+				r = input.nextInt();
+			} catch (java.util.InputMismatchException h) {
+				System.out.println("Ups... Solo numeros porfavor, intenta de nuevo:");
+				input.nextLine();
+				e=true;
+			}
+			if (r>0) {
+				e=false;
+			}else {
+				System.out.println("Solo numeros mayores a 0 por favor, intenta de nuevo:");
+				e=true;
+			}
+		}while(e);
+		return r;
+	}
+}
+
 
 class MetodosOrdenamiento{
 	
 	static class Burbuja{
 		static long tInicio, tFin;
+		
 		public static void ordenacionBurbuja1(int [] numeros) {
+		
+			int comparaciones=0, intercambios=0;
 			tInicio = System.nanoTime();
 			for (int i = 1;i<=numeros.length; i++) {
 				for (int j = 0;j<=numeros.length-i-1; j++) {
+					comparaciones = comparaciones + 1;
 					if(numeros[j+1]<numeros[j]) {
+						intercambios = intercambios +1;
 						int aux =numeros[j];
 						numeros[j] = numeros[j+1];
 						numeros[j+1] = aux;
@@ -19,10 +52,12 @@ class MetodosOrdenamiento{
 			
 			tFin = System.nanoTime()-tInicio;
 			System.out.println("Tiempo de ejecucion:" + tFin);
+			System.out.println("Numero de intercambios = " + intercambios);
+			System.out.println("Numeor de comparaciones = " + comparaciones);
 		}//Metodo OrdenacionBurbuja1
 	public void ordenacionBurbuja2(int [] numeros) {
 		
-	}
+	}//Metodo OrdenacionBurbuja2
 	public void ordenacionBurbuja3(int [] numeros) {
 		
 	}
@@ -45,21 +80,45 @@ public class PruebaMetodosDeOrdenamiento {
 
 	public static void main(String[] args) {
 		//Agregar MENU!
+		int opcion = 0;
+		do{
+			System.out.println("=================== MENU ===================");
+			System.out.println("Digite 1 para usar el metodo de ordenacion BURBUJA");
+			System.out.println("Digite 2 para ***SALIR***");
+			opcion = Correcion.validacion();
+			switch (opcion) {
+			case 1:
+				Random aleatorio = new Random(System.currentTimeMillis());
+				int numeros[] = new int[1000];
+				for (int i = 0; i <1000; i++) {
+					int intAletorio = aleatorio.nextInt(1000);
+					numeros[i]=intAletorio +1;
+				}
+				System.out.println("===== Vector DESORDENADO =====");
+				MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
+				MetodosOrdenamiento.Burbuja.ordenacionBurbuja1(numeros);
+				System.out.println("===== Vector ORDENADO =====");
+				MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
+				break;
+
+			case 2://Cambiar para la opcion SALIR
+				System.out.println("Gracias por usar");
+				break;
+			}
+			
+		}while (opcion!=2);//Usar aqui el numero de ***SALIR***
+		
+		
+		
 		
 		//LLenar el vector con 1 millon de datos aleatorios!!!!!
-		int numeros[] = {6,2,9,7,1,0,4};
 		
-		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
-		
-		MetodosOrdenamiento.Burbuja.ordenacionBurbuja1(numeros);
-		
-		MetodosOrdenamiento.Burbuja.mostrarVector(numeros);
 		
 		/*Cantidad de pasadas
 		 * Cantidad de comparaciones
 		 * cantidad de intercambios 
 		 */
-		
+	
 		
 	}
 
